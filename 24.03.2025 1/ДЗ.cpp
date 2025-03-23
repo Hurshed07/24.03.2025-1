10e8a749fdeaeb633421a6c8d141267142397773
Hurshed07 <hurshedashurboev932@gmail.com>
2025-03-24 04:55:22 +0700

    Update ДЗ.cpp

--- 24.03.2025 1/ДЗ.cpp
+++ 24.03.2025 1/ДЗ.cpp
@@ -1,3 +1,52 @@
+//1
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal 
+{
+private:
+    string name;
+    int weight;
+
+protected:
+    int age;
+
+public:
+    Animal(const string& animalName, int animalWeight)
+    {
+        this->name = animalName;
+        this->weight = animalWeight;
+        this->age = 0;
+    }
+
+    void print() const 
+    {
+        cout << "Animal Name: " << name << ", Weight: " << weight << " kg" << endl;
+    }
+
+    string getName() const { return name;}
+
+    int getWeight() const { return weight; }
+ 
+    void setAge(int animalAge) { age = animalAge; }
+
+    int getAge() const { return age; }
+};
+
+int main() 
+{
+    Animal myAnimal("Lion", 200);
+    myAnimal.print();
+
+    myAnimal.setAge(5);
+    cout << "Animal Age: " << myAnimal.getAge() << " years" << endl;
+}
+
+
+
+//2
 #include <iostream>
 #include <string>
 
@@ -46,11 +95,43 @@ public:
     }
 };
 
+class Cat : public Animal 
+{
+public:
+    Cat(const string& catName, int catWeight)
+        : Animal(catName, catWeight) {
+    }
+
+    void print() const 
+    {
+        cout << "Cat Name: " << this->getName() << ", Weight: " << this->getWeight() << " kg" << endl;
+    }
+};
+
+class Dog : public Animal 
+{
+public:
+    Dog(const string& dogName, int dogWeight)
+        : Animal(dogName, dogWeight) {
+    }
+
+    void print() const 
+    {
+        cout << "Dog Name: " << this->getName() << ", Weight: " << this->getWeight() << " kg" << endl;
+    }
+};
+
 int main() 
 {
-    Animal myAnimal("Lion", 200);
-    myAnimal.print();
+    Cat myCat("Whiskers", 5);
+    Dog myDog("Buddy", 25);
 
-    myAnimal.setAge(5);
-    cout << "Animal Age: " << myAnimal.getAge() << " years" << endl;
+    myCat.print();
+    myDog.print();
+
+    myCat.setAge(3);
+    myDog.setAge(4);
+
+    cout << "Cat Age: " << myCat.getAge() << " years" << endl;
+    cout << "Dog Age: " << myDog.getAge() << " years" << endl;
 }
